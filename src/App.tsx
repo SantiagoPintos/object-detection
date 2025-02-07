fb83285b3ca6cc55f3f3cdb5f521823d3981dc21
Santiago Pintos <spintos@outlook.es>
2025-02-07 12:51:33 -0300

    refactor: Move COLOURS array to a separate constants file for better organization

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,10 @@
 import { useEffect, useRef, useState } from 'react';
 import { env, AutoModel, AutoProcessor, RawImage } from '@xenova/transformers';
+import { COLOURS } from './constants/constants';
 import './App.css';
 
 env.allowLocalModels = false;
 env.backends.onnx.wasm.proxy = true;
-
-const COLOURS = [
-  "#EF4444", "#4299E1", "#059669",
-  "#FBBF24", "#4B52B1", "#7B3AC2",
-  "#ED507A", "#1DD1A1", "#F3873A",
-  "#4B5563", "#DC2626", "#1852B4",
-  "#18A35D", "#F59E0B", "#4059BE",
-  "#6027A5", "#D63D60", "#00AC9B",
-  "#E64A19", "#272A34"
-];
 
 const Control = ({ value, min, max, step, label, onChange }: ControlProps) => (
   <div>
